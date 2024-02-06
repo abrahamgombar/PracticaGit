01a4752f1a18b2e704e8122e4c081e54a0609c20
abrahamgombar <133754896+abrahamgombar@users.noreply.github.com>
2024-02-06 10:59:08 +0100

    Update prueba.java

--- src/practicaGit/prueba.java
+++ src/practicaGit/prueba.java
@@ -3,5 +3,6 @@ package practicaGit;
 public class prueba {
 	public static void main(String Args[]) {
 		System.out.println("Hola Mundo");
+		System.out.println("Linea agregado desde GitHub");
 	}
 }
